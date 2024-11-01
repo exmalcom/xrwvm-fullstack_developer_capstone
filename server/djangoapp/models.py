d3d04d95ddedf1a96d70204fce2fab4f40cbdd82
exmalcom <htcteambangis@gmail.com>
2024-11-02 02:34:46 +0800

    Fixing Lint errors models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -4,15 +4,13 @@ from django.db import models
 # from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-
 # Create your models here.
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 # - Name
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
-    # Other fields as needed
+    description = models.TextField()  # Other fields as needed
 
     def __str__(self):
         return self.name  # Return the name as the string representation
@@ -21,24 +19,25 @@ class CarMake(models.Model):
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
     name = models.CharField(max_length=100)
-    
+
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
         ('WAGON', 'Wagon'),
         # Add more choices as required
     ]
-    
+
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    
+
     year = models.IntegerField(
         default=2023,
         validators=[
             MaxValueValidator(2023),
             MinValueValidator(2015),
         ]
-    )
-    # Other fields as needed
+    )  # Other fields as needed
 
     def __str__(self):
         return self.name  # Return the name as the string representation
+
+# Ensure there is a newline at the end of the file
