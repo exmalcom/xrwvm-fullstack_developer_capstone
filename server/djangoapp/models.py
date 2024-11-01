8f582dd02392b07e06f7b0cc19f19630d3a2cad5
exmalcom <htcteambangis@gmail.com>
2024-11-02 02:40:27 +0800

    Fix 3 models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -2,10 +2,7 @@ from django.db import models
 # from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()  # Other fields as needed
