e874230c6e59814ae5bbed394091c000083d28d3
exmalcom <htcteambangis@gmail.com>
2024-11-02 01:53:14 -0400

    I use for IBM labs

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = 'django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'dealershipcar.com']
-CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'http://dealershipcar.com']
+ALLOWED_HOSTS = ['localhost', '']
+CSRF_TRUSTED_ORIGINS = ['''https://htcteambangi-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai''']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
