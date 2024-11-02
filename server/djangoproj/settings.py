f9e9e05ca03dab9f5c9460edabdc16fc68504641
exmalcom <htcteambangis@gmail.com>
2024-11-02 01:10:25 -0400

    Full Stack Complete

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = 'django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['http://localhost:8000']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'dealershipcar.com']
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'http://dealershipcar.com']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
